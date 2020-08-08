737e69253d7b2e33a9f5a591381ae69c3c842e65
Florian Fetz <florian.fetz@web.de>
2020-08-08 11:36:10 +0200

    Improved monkey test, but still not good enough for productive use

--- tests/test_MonkeyTest.py
+++ tests/test_MonkeyTest.py
@@ -3,17 +3,41 @@ import threading
 from time import sleep
 from unittest import TestCase
 
+import asn1tools
 import wx
 
 import asn1editor
 
 
-def actions(event_handler: wx.EvtHandler):
+def actions(main_window: asn1editor.wxPython.MainWindow):
+    def get_children(window: wx.Window):
+        my_children = window.GetChildren()
+        if my_children is not None:
+            their_children = []
+            for my_child in my_children:
+                their_children += get_children(my_child)
+            return list(my_children) + their_children
+        else:
+            return []
+
     sleep(1)
-    for i in range(1000):
+
+    children = get_children(main_window)
+    key_codes = [wx.WXK_TAB, wx.WXK_DOWN, wx.WXK_UP, wx.WXK_LEFT, wx.WXK_RIGHT, wx.WXK_SPACE, wx.WXK_SELECT] + [c for c in range(ord('1'), ord('9'))]
+
+    for i in range(10000):
         key_event = wx.KeyEvent(wx.EVT_CHAR.typeId)
-        key_event.SetKeyCode(random.choice([wx.WXK_TAB, wx.WXK_DOWN, wx.WXK_UP, wx.WXK_LEFT, wx.WXK_RIGHT]))
+        key_event.SetKeyCode(random.choice(key_codes))
+
+        child = random.choice(children)
+        event_handler = child.GetEventHandler()
         wx.PostEvent(event_handler, key_event)
+    try:
+        main_window.save_data_to_file('test.json')
+    except asn1tools.ConstraintsError:
+        pass
+
+    main_window.Close(True)
 
 
 class MonkeyTest(TestCase):
@@ -27,9 +51,10 @@ class MonkeyTest(TestCase):
 
         for spec, type_ in test_types:
             main_window.load_spec(spec, type_)
-            action_thread = threading.Thread(target=actions, args=[main_window.GetEventHandler()])
+            action_thread = threading.Thread(target=actions, args=[main_window])
             action_thread.start()
             main_window.Show()
             app.MainLoop()
-            main_window.save_data_to_file('test.json')
             action_thread.join(timeout=0.0)
+
+        app.Destroy()
