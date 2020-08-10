820c99841db8cb5d8dad94ca613130cfa8a20089
Florian Fetz <florian.fetz@web.de>
2020-08-10 11:36:27 +0200

    Settings data is now stored in appdata directory or in home directory (win/linux)

--- asn1editor/wxPython/Settings.py
+++ asn1editor/wxPython/Settings.py
@@ -1,12 +1,27 @@
 import json
+import os
+import sys
 
 settings = {}
+
+
+def _get_filename():
+    if sys.platform == 'win32':
+        win_path = os.path.expandvars(r'%APPDATA%/asn1editor')
+        os.makedirs(win_path, exist_ok=True)
+        return os.path.join(win_path, 'settings.json')
+    elif sys.platform.startswith('linux'):
+        home = os.path.expanduser('~/.local/share/asn1editor')
+        os.makedirs(home, exist_ok=True)
+        return os.path.join(home, 'settings.json')
+    else:
+        return 'settings.json'
 
 
 def load():
     global settings
     try:
-        with open('settings.json', 'r') as f:
+        with open(_get_filename(), 'r') as f:
             settings = json.load(f)
     except FileNotFoundError:
         settings = {}
@@ -16,5 +31,5 @@ def load():
 
 def save():
     global settings
-    with open('settings.json', 'w+') as f:
+    with open(_get_filename(), 'w+') as f:
         f.write(json.dumps(settings))
