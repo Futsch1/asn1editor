bc9b5cd9aaac426e3b848b9a328d2bd85590c131
Florian Fetz <florian.fetz@web.de>
2020-12-18 17:03:17 +0100

    Added test for WxPython editor from init

---
+++ tests/test_WxPythonFromInit.py
@@ -0,0 +1,22 @@
+import threading
+from time import sleep
+from unittest import TestCase
+
+import wx
+
+import asn1editor
+
+
+def delay():
+    sleep(1)
+    wx.GetApp().GetTopWindow().Close(True)
+    wx.GetApp().ExitMainLoop()
+
+
+class WxPythonFromInit(TestCase):
+    @staticmethod
+    def test_open():
+        action_thread = threading.Thread(target=delay, args=[])
+        action_thread.start()
+
+        asn1editor._wx_python_editor()
