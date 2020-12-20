8e0f69eed85a1a3a967132eb6d38c66ea89584e0
Florian Fetz <florian.fetz@googlemail.com>
2020-12-20 10:14:00 +0100

    Fixed unit tests in Linux environment

--- tests/test_LoadExample.py
+++ tests/test_LoadExample.py
@@ -2,8 +2,6 @@ import threading
 from time import sleep
 from unittest import TestCase
 
-import wx
-
 import asn1editor
 from asn1editor.wxPython.ViewSelect import ViewType
 from tests import testHelper
@@ -12,33 +10,32 @@ from tests.TestPlugin import TestPlugin
 
 def delay(main_window: asn1editor.wxPython.MainWindow):
     sleep(1)
-
     main_window.Close(True)
-    wx.GetApp().ExitMainLoop()
 
 
 class LoadExample(TestCase):
-    @staticmethod
-    def test_tree():
-        LoadExample.__test_internal(ViewType.TREE)
 
-    @staticmethod
-    def test_group():
-        LoadExample.__test_internal(ViewType.GROUPS)
+    def setUp(self) -> None:
+        self.app = testHelper.get_wx_app()
 
-    @staticmethod
-    def __test_internal(v: ViewType):
+    def tearDown(self) -> None:
+        self.app.Destroy()
+
+    def test_tree(self):
+        self.__test_internal(ViewType.TREE)
+
+    def test_group(self):
+        self.__test_internal(ViewType.GROUPS)
+
+    def __test_internal(self, v: ViewType):
         # noinspection PyUnusedLocal
-        app = testHelper.get_wx_app()
         main_window = asn1editor.wxPython.MainWindow(plugins=[TestPlugin()])
+
+        main_window.load_spec('example/example.asn', 'EXAMPLE.Sequence')
         main_window.select_view(v)
 
-        test_types = [('example/example.asn', 'EXAMPLE.Sequence')]
-
-        for spec, type_ in test_types:
-            main_window.load_spec(spec, type_)
-            action_thread = threading.Thread(target=delay, args=[main_window])
-            action_thread.start()
-            main_window.Show()
-            app.MainLoop()
-            action_thread.join(timeout=0.0)
+        action_thread = threading.Thread(target=delay, args=[main_window])
+        # action_thread.start()
+        main_window.Show()
+        self.app.MainLoop()
+        action_thread.join(timeout=1.0)
