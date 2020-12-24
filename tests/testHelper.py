1516e53bbec63f9af4d67d7db7a18f8a4543cc19
Florian Fetz <florian.fetz@googlemail.com>
2020-12-24 10:18:26 +0100

    Another fix for unit tests

--- tests/testHelper.py
+++ tests/testHelper.py
@@ -1,8 +1,13 @@
+import time
+
 import wx
 
 
 def get_wx_app() -> wx.App:
     app = wx.GetApp()
-    if app is None:
-        app = wx.App()
+    if app is not None:
+        app.Destroy()
+        time.sleep(1)
+
+    app = wx.App()
     return app
