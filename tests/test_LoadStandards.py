faf5624e3a7a98b37e4108d88f106f17859ec9a9
Florian Fetz <florian.fetz@googlemail.com>
2020-12-28 15:35:51 +0100

    Improvements in unit tests

--- tests/test_LoadStandards.py
+++ tests/test_LoadStandards.py
@@ -1,9 +1,11 @@
 import glob
 import os
+import random
 from unittest import TestCase
 
 import asn1editor
 from asn1editor.ASN1SpecHandler import ASN1SpecHandler
+from asn1editor.wxPython.ViewSelect import ViewType, TagInfo
 from tests import TestHelper
 
 
@@ -14,6 +16,8 @@ class LoadStandardsTest(TestCase):
             print(f'Loading types from {file}')
             asn1_handler = ASN1SpecHandler(file)
             types = asn1_handler.get_types()
+            if os.getenv('ASN1EDITOR_QUICK_TESTS') is not None:
+                types = random.sample(types, 5)
             for type_ in types:
                 if type_ in tested_types:
                     continue
@@ -22,11 +26,10 @@ class LoadStandardsTest(TestCase):
                 self.assertEqual(main_window.get_spec_filename(), file)
                 self.assertEqual(main_window.get_typename(), type_)
                 tested_types.add(type_)
-                if os.getenv('ASN1EDITOR_QUICK_TESTS') is not None:
-                    break
 
     def test_load_standards(self):
         # noinspection PyUnusedLocal
         app = TestHelper.get_wx_app()
         main_window = asn1editor.wxPython.MainWindow()
+        main_window.select_view_and_tag_info(ViewType.GROUPS, TagInfo.LABELS)
         self.__load_standards(main_window)
